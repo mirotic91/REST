60499647c6a303cb6f574033d513e0b887acc3d4
jonguk park <jupark@jongukui-MacBookPro.local>
2019-05-07 08:38:28 +0900

    Event Domain

---
+++ src/test/java/com/mirotic/demorestapi/events/EventTest.java
@@ -0,0 +1,30 @@
+package com.mirotic.demorestapi.events;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class EventTest {
+
+    @Test
+    public void builder() {
+        Event event = Event.builder()
+                .name("Spring REST API")
+                .description("REST API development with Spring")
+                .build();
+        assertThat(event).isNotNull();
+    }
+
+    @Test
+    public void javaBean() {
+        String name = "Spring REST API";
+        String description = "REST API development with Spring";
+
+        Event event = new Event();
+        event.setName(name);
+        event.setDescription(description);
+
+        assertThat(event.getName()).isEqualTo(name);
+        assertThat(event.getDescription()).isEqualTo(description);
+    }
+}
