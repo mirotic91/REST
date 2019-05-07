c7cc03b5fe7e5734c62acf93463cd635b4f307fa
jonguk park <jupark@jongukui-MacBookPro.local>
2019-05-07 13:00:28 +0900

    Add create events api

--- src/main/java/com/mirotic/demorestapi/events/Event.java
+++ src/main/java/com/mirotic/demorestapi/events/Event.java
@@ -7,28 +7,48 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.time.LocalDateTime;
 
-@Getter
-@Setter
+@Entity
+@Getter @Setter
 @EqualsAndHashCode(of = "id")
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Event {
 
+    @Id @GeneratedValue
     private Integer id;
+
     private String name;
+
     private String description;
+
     private LocalDateTime beginEnrollmentDateTime;
+
     private LocalDateTime closeEnrollmentDateTime;
+
     private LocalDateTime beginEventDateTime;
+
     private LocalDateTime endEventDateTime;
+
     private String location; // optional
+
     private int basePrice; // optional
+
     private int maxPrice; // optional
+
     private int limitOfEnrollment;
+
     private boolean offline;
+
     private boolean free;
+
+    @Enumerated(EnumType.STRING)
     private EventStatus eventStatus = EventStatus.DRAFT;
 }
--- src/test/java/com/mirotic/demorestapi/events/EventControllerTests.java
+++ src/test/java/com/mirotic/demorestapi/events/EventControllerTests.java
@@ -1,15 +1,24 @@
 package com.mirotic.demorestapi.events;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -19,11 +28,38 @@ public class EventControllerTests {
     @Autowired
     MockMvc mockMvc;
 
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @MockBean
+    EventRepository eventRepository;
+
     @Test
     public void createEvent() throws Exception {
+        Event event = Event.builder()
+                .id(111) //
+                .name("Spring")
+                .description("REST API")
+                .beginEnrollmentDateTime(LocalDateTime.of(2019, 5, 7, 12, 30))
+                .closeEnrollmentDateTime(LocalDateTime.of(2019, 5, 8, 12, 30))
+                .beginEventDateTime(LocalDateTime.of(2019, 5, 13, 12, 0))
+                .endEventDateTime(LocalDateTime.of(2019, 5, 17, 18, 0))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("강남역 D2")
+                .build();
+
+        Mockito.when(eventRepository.save(event)).thenReturn(event);
+
         mockMvc.perform(post("/api/events/")
                     .contentType(MediaType.APPLICATION_JSON)
-                    .accept(MediaTypes.HAL_JSON))
-                .andExpect(status().isCreated());
+                    .accept(MediaTypes.HAL_JSON)
+                    .content(objectMapper.writeValueAsString(event)))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(header().exists(HttpHeaders.LOCATION))
+                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_UTF8_VALUE))
+                .andExpect(jsonPath("id").exists());
     }
 }
