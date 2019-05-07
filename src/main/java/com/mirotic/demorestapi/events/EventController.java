359fe4e1ee8797f45fd5f2c67e14178c19cc5e9e
jonguk <jonguk1221@gmail.com>
2019-05-07 13:00:28 +0900

    Add create events api

---
+++ src/main/java/com/mirotic/demorestapi/events/EventController.java
@@ -0,0 +1,28 @@
+package com.mirotic.demorestapi.events;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+
+@RestController
+@RequestMapping(value = "/api/events", produces = MediaTypes.HAL_JSON_UTF8_VALUE)
+@RequiredArgsConstructor
+public class EventController {
+
+    private final EventRepository eventRepository;
+
+    @PostMapping
+    public ResponseEntity createEvent(@RequestBody Event event) {
+        Event createdEvent = eventRepository.save(event);
+        URI createdUri = linkTo(EventController.class).slash(createdEvent.getId()).toUri();
+        return ResponseEntity.created(createdUri).body(createdEvent);
+    }
+}
